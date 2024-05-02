7a7ac2e3cd191d0b785c69d658faaa9076c8c7f2
Mr_Artur <panosyanartur31@gmail.com>
2024-05-03 00:14:46 +0400

    Add edge-service

---
+++ src/test/java/am/itspace/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
